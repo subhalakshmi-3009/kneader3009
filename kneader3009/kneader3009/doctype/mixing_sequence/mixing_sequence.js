c75e29a33e33d172db31b0b3e10b91ee2b138df4
subhalakshmi-3009 <subha@alphaworkz.com>
2025-11-22 12:25:48 +0000

    Initial commit of kneader3009 app with Mixing Sequence doctypes

---
+++ kneader3009/kneader3009/doctype/mixing_sequence/mixing_sequence.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, subha and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mixing Sequence", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ kneader3009/kneader3009/doctype/mixing_sequence/test_mixing_sequence.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, subha and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMixingSequence(FrappeTestCase):
+	pass
